24592bcd7e7dac09e44f0a0613ffd19a491fef30
Giles Roadnight <giles.roadnight@gmail.com>
2022-03-23 08:23:08 +0000

    Add version number to window title

--- test-harness/app/app.module.ts
+++ test-harness/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import projectPackage from '../../package.json';
 
 import { AppComponent } from './app.component';
 
@@ -9,4 +10,8 @@ import { AppComponent } from './app.component';
     providers: [],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+    constructor() {
+        window.document.title = `ble-hrm Test Harness ${projectPackage.version}`;
+    }
+}
